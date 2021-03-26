1f509ba15d928dbce3a1a4704a76062d3569565d
Alok <Alok@DESKTOP-FGNVT4U>
2021-03-26 17:00:15 +0530

    first code

---
+++ SpringBootRestGit/src/main/java/com/reed/BootStarter.java
@@ -0,0 +1,11 @@
+package com.reed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootStarter {
+public static void main(String[] args) {
+	SpringApplication.run(BootStarter.class, args);
+}
+}
